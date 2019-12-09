809c7b8c31c1f659658cb76b14cf3fecea74b717
Божидар Димов <bojko958@abv.bg>
2019-12-09 22:47:24 +0200

    added wave effect to map

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,11 @@ const stats = new Stats();
 
 
 const light = new LightWithBody(2, 150);//new THREE.PointLight(0xffffff, 25, 100);
-light.setPosition(0, 20, 0);
+light.setPosition(0, 40, 0);
 scene.add(...light.objects());
 
-camera.position.z = 55;
-camera.position.y = 30;
+camera.position.z = 25;
+camera.position.y = 15;
 camera.position.x = 0;
 camera.lookAt(new THREE.Vector3(0, 0, -5));
 
@@ -40,6 +40,7 @@ const cubes = range(1, 1024)
 
 scene.add(...cubes);
 
+
 const lineMaterial1 = new THREE.LineBasicMaterial({ color: 0x312DB3 });
 const lineMaterial2 = new THREE.LineBasicMaterial({ color: 0xFFA573 });
 const lineMaterial3 = new THREE.LineBasicMaterial({ color: 0x50CC33 });
@@ -56,30 +57,31 @@ zAxisGeometry.vertices.push(new THREE.Vector3(0, 0, 10000));
 const xAxis = new THREE.Line(xAxisGeometry, lineMaterial2);
 const yAxis = new THREE.Line(yAxisGeometry, lineMaterial1);
 const zAxis = new THREE.Line(zAxisGeometry, lineMaterial3);
-scene.add(xAxis);
-scene.add(yAxis);
-scene.add(zAxis);
+// scene.add(xAxis);
+// scene.add(yAxis);
+// scene.add(zAxis);
 
-const uniforms = {
-  delta: 15 * Math.PI / 180
-};
+// const uniforms = {
+//   delta: 15 * Math.PI / 180
+// };
 
-const customMat = new THREE.ShaderMaterial({
-  uniforms,
-  vertexShader: Tile.vertexShaderSource,
-  fragmentShader: Tile.fragmentShaderSource
-});
+// const customMat = new THREE.ShaderMaterial({
+//   uniforms,
+//   vertexShader: Tile.vertexShaderSource,
+//   fragmentShader: Tile.fragmentShaderSource
+// });
 
-const customGeom = new THREE.BoxBufferGeometry(1, 1, 1);
-const oscillator = new THREE.Mesh(customGeom, customMat);
-oscillator.position.set(0, 10, 0);
-scene.add(oscillator);
+// const customGeom = new THREE.BoxBufferGeometry(1, 1, 1);
+// const oscillator = new THREE.Mesh(customGeom, customMat);
+// oscillator.position.set(0, 10, 0);
+// scene.add(oscillator);
 
 function main(time: number) {
   requestAnimationFrame(main);
   stats.update();
-  uniforms.delta = time;
-
+  cubes.forEach((cube, index) => {
+    cube.position.y = 35 * Math.sin(index / cubes.length * 16 + (time + index) / 700) / 100;
+  });
   renderer.render(scene, camera);
 }
 
